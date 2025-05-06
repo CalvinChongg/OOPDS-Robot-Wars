b9bcb432d5771052bbdef9c8cec31f913e54f164
DannyYap0215 <dannychiyi@gmail.com>
2025-05-06 15:10:58 +0800

    edit information for member

--- main.cpp
+++ main.cpp
@@ -1,3 +1,28 @@
+//*********************************************************  
+// Program: main.cpp  
+// Course: CCP6124 OOPDS 
+// Lecture Class: TC2L
+// Tutorial Class: TT5L
+// Trimester: 2510
+// Member_1: 242UC244JT | CHONG MENG HANG | chong.meng.hang@student.mmu.edu.my | 016-8711296
+// Member_2: 242UC244PF | TAN YONG XIN | tan.yong.xin@student.mmu.edu.my | 012-6556505
+// Member_3: 242UC244S6 | THAM MEI TING | tham.mei.ting@student.mmu.edu.my | 017-3268006
+// Member_4: 242UC244KX | YAP CHI YI | yap.chi.yi@student.mmu.edu.my | 018-2694514	 
+//*********************************************************
+// Task Distribution
+// Member_1: CHONG MENG HANG
+// Member_2: TAN YONG XIN
+// Member_3: THAM MEI TING 
+// Member_4: YAP CHI YI
+// ********************************************************
+
+// Filename examples
+// {
+// TC2L_G04_main.cpp
+// fileInput1.txt, fileInput2.txt, fileOutput1.txt, fileOutput2.txt
+// TC2L_G04_report.docx
+// }
+
 #include <iostream>
 #include <string>
 #include <cmath>
