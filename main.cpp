2ce22df5c91ac7fd9407ce6b12107140fa0fd6f9
DannyYap0215 <dannychiyi@gmail.com>
2025-05-20 15:51:39 +0800

    display battlefield is now working fine

--- main.cpp
+++ main.cpp
@@ -225,20 +225,34 @@ public:
 
         battlefield_.clear();  // clear previous contents
 
-        while (getline(GameFile, line, ',')) {
+        while (getline(GameFile, line)) {
             // Remove any carriage return or trailing spaces
             line.erase(0, line.find_first_not_of(" \t"));
             line.erase(line.find_last_not_of(" \t") + 1);
 
             vector<string> row;
             row.push_back(line);  // Treat entire line as a single column
+
+            // check input for map battlefield size
+            if (line.find("M by N:") != string::npos) {
+                int rows, cols;
+                sscanf(line.c_str(), "M by N: %d %d", &rows, &cols);
+                cout << "rows = " << rows << ", cols = " << cols << endl;
+
+                battlefield_.resize(rows, vector<string>(cols, ""));
+            }
+
+            // check input for amount of turns 
+            if (line.find("turns:") != string::npos) {
+                int NumOfTurns;
+                sscanf(line.c_str(), "turns: %d", &NumOfTurns);
+                cout << "turns = " << NumOfTurns<<endl;
+            }
+
+
         }
 
-        string cell = "M by N: 10 15";
-        int rows, cols;
-        sscanf(cell.c_str(), "M by N: %d %d", &rows, &cols);
-
-        battlefield_.resize(rows, vector<string>(cols, ""));
+        
 
         GameFile.close();
     }
